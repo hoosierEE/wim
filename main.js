8bae079c572683a05199a834646245f4441f3241
hoosieree <ashroyer@140-182-219-199.dhcp-bl.indiana.edu>
2016-08-15 17:18:02 -0400

    add keyup and keydown

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 /* wim -- modal text editor */
 'use strict';
-const ctx=document.getElementById('c').getContext('2d');
 
-const testwrite=(c)=>{
-    c.clearRect(0,0,c.canvas.width,c.canvas.height);
-    c.font='16px monospace';
-    c.fillText('hello canvas on a high dpi screen',20,30);
-};
+const ctx=document.getElementById('c').getContext('2d');
 
 /* TODO keyboard input
    - escape sequences: Ctrl+], 'fd' in rapid sequence, and <ESC>
@@ -14,7 +9,14 @@ const testwrite=(c)=>{
    - motions aren't recorded
    - edits are saved to a fixed-length queue for undo purposes
 */
-const rsz=(c)=>{
+
+const testwrite=c=>{
+    c.clearRect(0,0,c.canvas.width,c.canvas.height);
+    c.font='16px monospace';
+    c.fillText('hello canvas on a high dpi screen',20,30);
+};
+
+const rsz=c=>{
     let dpr=window.devicePixelRatio;
     const h=window.innerHeight, w=window.innerWidth;
     [c.canvas.height,c.canvas.width]=[h,w].map(x=>(dpr?dpr:1)*x);
@@ -23,5 +25,13 @@ const rsz=(c)=>{
     testwrite(c);
 };
 
-window.addEventListener('resize',()=>{rsz(ctx);});
-window.addEventListener('DOMContentLoaded',()=>{rsz(ctx);});
+window.addEventListener('keydown',e=>{
+    const k=[1, e.key, e.timeStamp].concat('altKey ctrlKey metaKey shiftKey'.split(' ').map(x=>~~e[x]));
+    console.log(k)
+});
+window.addEventListener('keyup',e=>{
+    const k=[0, e.key, e.timeStamp].concat('altKey ctrlKey metaKey shiftKey'.split(' ').map(x=>~~e[x]));
+    console.log(k)
+});
+window.addEventListener('resize',()=>rsz(ctx));
+window.addEventListener('DOMContentLoaded',()=>rsz(ctx));
