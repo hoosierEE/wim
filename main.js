4f38ca83a290183e7f67e8437254e9be44a7733a
hoosieree <ashroyer@140-182-219-199.dhcp-bl.indiana.edu>
2016-08-19 17:04:27 -0400

    filtering some keys

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /* wim -- modal text editor
    Haskell-inspired type signatures/comments
    Rust-inspired type names:  (uN: unsigned N-bit int), (s: string), (f: float)
- */
+*/
 'use strict';
 /* Utils */
 /* zip : [(x,y)] -> ([x],[y]) */
@@ -20,11 +20,16 @@ const testwrite=c=>{
     c.fillText(lines.reduce((x,y)=>x+y+'\n',''),20,30);
 };
 
-/* TODO keyboard input
-   - inputs accumulate until they can be recognized as 'operations', then are pushed to history stack
-   - 'insert mode' inputs aren't saved */
+/* keyboard input -- RawKeys accumulate until:
+   - an editing operation occurs (push op to history stack, clear RawKeys)
+   - a mode change (normal -> insert) occurs (accumulate string instead of RawKeys)
+   - macro recording begins
+   - there are 100+ of them
+*/
+
+/* RawKeys : [s, u64, u1, u4] */
 /* key_transform : KeyboardEvent -> [UnicodeKey, MillisecondTimestamp, IsKeyDown, ModifierCode]
-   key_transform : o -> [s, u64, u1, u4] */
+   key_transform : o -> RawKeys */
 const key_transform=key_event=>[
     key_event.key,
     key_event.timeStamp|0,
@@ -34,12 +39,33 @@ const key_transform=key_event=>[
         .reduce((x,y,i,arr)=>x+y*Math.pow(2,arr.length-1-i),0)
 ];
 
-/* down_keys : [s, u64, u1, u4] -> u1 */
-const down_keys=keys=>keys.filter(x=>x[2]);
+/* pressed_keys : RawKeys -> RawKeys */
+const pressed_keys=keys=>keys.filter(x=>x[2]);
 
-let Keys=[];
-window.addEventListener('keydown',x=>Keys.push(key_transform(x)));
-window.addEventListener('keyup',x=>Keys.push(key_transform(x)));
+const Commands={
+    //    Escape: escape,
+    //    Tab: insert_tab,
+};
+const get_command=keys=>{
+    // parse escape codes
+    return keys;
+};
+
+let RawKeys=[];
+window.addEventListener('keydown',x=>{
+    const kt=key_transform(x),
+          pd=[ /* prevent defaults for these chords */
+              ['Escape',-1],
+              ['l',2],
+              ['l',4],
+          ].find(i=>i[0]===kt[0]&&(i[1]<0||i[1]===kt[3]));
+    if(pd!=null){x.preventDefault();console.log(`prevented ${pd}`)}
+    RawKeys.push(kt);
+
+    const pk=pressed_keys(RawKeys);
+    console.table(get_command(pk).slice(-4));
+});
+window.addEventListener('keyup',x=>RawKeys.push(key_transform(x)));
 
 const DEBUG=1;
 const resize_handler=c=>{
@@ -48,7 +74,6 @@ const resize_handler=c=>{
     [c.canvas.style.height,c.canvas.style.width]=[h,w].map(x=>x+'px');
     c.scale(dpr,dpr);
     if(DEBUG){
-        //console.log(`DEBUG: ${DEBUG}`);
         testwrite(c);
     }
 };
