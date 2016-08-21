989a65be39692fbc49c250f9e5331de82fdc41a2
hoosierEE <ashroyer@gmail.com>
2016-08-21 12:15:19 -0400

    add architecture.org and contributing.org as documentation

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
 /* wim -- modal text editor
-   Haskell-inspired type signatures/comments
-   Rust-inspired type names:  (uN: unsigned N-bit int), (s: string), (f: float)
 */
 'use strict';
-/* Utils */
-/* zip : [(x,y)] -> ([x],[y]) */
-const zip=xs=>xs[0].map((_,y)=>xs.map(x=>x[y]));
-/* curry : ((x,y) -> z) -> (x -> y -> z) */
-const curry=(f,...x)=>f.length>x.length?(...y)=>curry(f,...x,...y):f(...x);
 
 const testwrite=c=>{
     c.clearRect(0,0,c.canvas.width,c.canvas.height);
@@ -27,52 +20,41 @@ const testwrite=c=>{
    - there are 100+ of them
 */
 
-/* RawKeys : [s, u64, u1, u4] */
-/* key_transform : KeyboardEvent -> [UnicodeKey, MillisecondTimestamp, IsKeyDown, ModifierCode]
-   key_transform : o -> RawKeys */
+/* RawKey : (s, u64, u1, u4)
+   key_transform : KeyboardEvent -> [UnicodeKey, MillisecondTimestamp, IsKeyDown, ModifierCode]
+   key_transform : o -> RawKey
+*/
 const key_transform=key_event=>[
     key_event.key,
     key_event.timeStamp|0,
     key_event.type=='keydown'|0,
     ['altKey','ctrlKey','metaKey','shiftKey']
         .map(y=>key_event[y]|0)
-        .reduce((x,y,i,arr)=>x+y*Math.pow(2,arr.length-1-i),0)
+        .reduce((x,y,i,arr)=>x+y*Math.pow(2,arr.length-1-i),0) /* 0-15 */
 ];
 
-/* pressed_keys : RawKeys -> RawKeys */
+/* pressed_keys : [RawKey] -> [RawKey] */
 const pressed_keys=keys=>keys.filter(x=>x[2]);
-
-const Commands={
-    //    Escape: escape,
-    //    Tab: insert_tab,
-};
-const get_command=keys=>{
-    // parse escape codes
-    return keys;
-};
 
 let RawKeys=[];
 window.addEventListener('keydown',x=>{
-    const kt=key_transform(x),
-          pd=[ /* prevent defaults for these chords */
-              ['Escape',-1],
-              ['l',2],
-              ['l',4],
-          ].find(i=>i[0]===kt[0]&&(i[1]<0||i[1]===kt[3]));
-    if(pd!=null){x.preventDefault();console.log(`prevented ${pd}`)}
+    const kt=key_transform(x);
+    [/* preventDefault() for the following chords */
+        ['l',2],/* Meta l */
+        ['l',4],/* Ctrl l */
+        ['Escape',null],/* Escape */
+    ].find(i=>i[0]===kt[0]&&(i[1]===kt[3]||!i[1]))&&x.preventDefault();
     RawKeys.push(kt);
-
-    const pk=pressed_keys(RawKeys);
-    console.table(get_command(pk).slice(-4));
+    console.table(pressed_keys(RawKeys).slice(-4));
 });
 window.addEventListener('keyup',x=>RawKeys.push(key_transform(x)));
 
-const DEBUG=1;
 const resize_handler=c=>{
     const dpr=window.devicePixelRatio, h=window.innerHeight, w=window.innerWidth;
     [c.canvas.height,c.canvas.width]=[h,w].map(x=>dpr*x);
     [c.canvas.style.height,c.canvas.style.width]=[h,w].map(x=>x+'px');
     c.scale(dpr,dpr);
+    let DEBUG=1;
     if(DEBUG){
         testwrite(c);
     }
