3c7dc1d27d958ac7a4906788b8b01815a075ee5f
hoosierEE <ashroyer@gmail.com>
2016-08-21 17:13:38 -0400

    more docs

--- main.js
+++ main.js
@@ -13,13 +13,6 @@ const testwrite=c=>{
     c.fillText(lines.reduce((x,y)=>x+y+'\n',''),20,30);
 };
 
-/* keyboard input -- RawKeys accumulate until:
-   - an editing operation occurs (push op to history stack, clear RawKeys)
-   - a mode change (normal -> insert) occurs (accumulate string instead of RawKeys)
-   - macro recording begins
-   - there are 100+ of them
-*/
-
 /* RawKey : (s, u64, u1, u4)
    key_transform : KeyboardEvent -> [UnicodeKey, MillisecondTimestamp, IsKeyDown, ModifierCode]
    key_transform : o -> RawKey
@@ -36,28 +29,25 @@ const key_transform=key_event=>[
 /* pressed_keys : [RawKey] -> [RawKey] */
 const pressed_keys=keys=>keys.filter(x=>x[2]);
 
-let RawKeys=[];
+let RawKeys=[],DownKeys={};
 window.addEventListener('keydown',x=>{
     const kt=key_transform(x);
-    [/* preventDefault() for the following chords */
-        ['l',2],/* Meta l */
-        ['l',4],/* Ctrl l */
-        ['Escape',null],/* Escape */
-    ].find(i=>i[0]===kt[0]&&(i[1]===kt[3]||!i[1]))&&x.preventDefault();
+    DownKeys[x.key]=true;
+    if(!(kt[0]=='I'&&kt[3]==5||kt[3]==10)){x.preventDefault()}
     RawKeys.push(kt);
     console.table(pressed_keys(RawKeys).slice(-4));
 });
-window.addEventListener('keyup',x=>RawKeys.push(key_transform(x)));
+window.addEventListener('keyup',x=>{
+    DownKeys[x.key]=false;
+    RawKeys.push(key_transform(x));
+});
 
 const resize_handler=c=>{
     const dpr=window.devicePixelRatio, h=window.innerHeight, w=window.innerWidth;
     [c.canvas.height,c.canvas.width]=[h,w].map(x=>dpr*x);
     [c.canvas.style.height,c.canvas.style.width]=[h,w].map(x=>x+'px');
     c.scale(dpr,dpr);
-    let DEBUG=1;
-    if(DEBUG){
-        testwrite(c);
-    }
+    testwrite(c); // DEBUG
 };
 const ctx=document.getElementById('c').getContext('2d');
 window.addEventListener('resize',()=>resize_handler(ctx));
