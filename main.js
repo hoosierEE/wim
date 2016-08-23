b97a7f38bd488e84ff11aaee3ba34ef6010d9661
hoosieree <ashroyer@140-182-219-199.dhcp-bl.indiana.edu>
2016-08-23 13:28:56 -0400

    noticed that setting the font forces style recalculation -- not sure if that is bad

--- main.js
+++ main.js
@@ -1,51 +1,54 @@
-/* wim -- modal text editor
-*/
+/* wim -- modal text editor */
 'use strict';
 
-const testwrite=c=>{
+/* Testing -- write a string to the canvas */
+const do_render=c=>{
     c.clearRect(0,0,c.canvas.width,c.canvas.height);
-    c.font='16px monospace';
     const lines=[
         'TODO keyboard input',
         '- inputs accumulate until they can be recognized as \'operations\', then are pushed to history stack',
         '- \'insert mode\' inputs aren\'t saved.'
-    ];
-    c.fillText(lines.reduce((x,y)=>x+y+'\n',''),20,30);
+    ].join('\n');
+    c.fillText(lines,20,30);
 };
 
-/* key_transform : KeyboardEvent:o -> (UnicodeKey:s, MillisecondTimestamp:i, IsKeyDown:u1, ModifierCode:u4)
-   key_transform : key_event -> RawKey */
-const key_transform=key_event=>({
+/* Events -- keyboard and mouse */
+
+/* key_filter : KeyboardEvent:o -> RawKey:o */
+const key_filter=key_event=>({
     key:key_event.key,
     code:key_event.code,
     timestamp:key_event.timeStamp|0,
-    type:key_event.type=='keydown'|0,
+    type:key_event.type==='keydown'|0,
     modifier:['altKey','ctrlKey','metaKey','shiftKey']
-        .map(y=>key_event[y]|0)
-        .reduce((x,y,i,arr)=>x+y*Math.pow(2,arr.length-1-i),0) /* 0-15 */
+        .map(y=>key_event[y]|0).reduce((x,y,i,arr)=>x+y*Math.pow(2,arr.length-1-i),0) /* 0-15 */
 });
 
-window.addEventListener('keydown',x=>{
-    const kt=key_transform(x);
-    DownKeys[kt.code]=true;
-    if(!((kt.key=='I'&&kt.modifier==5)||(kt.modifier==10&&kt.code=='KeyI'))){x.preventDefault()}
-    RawKeys.push(kt);
-    console.table(RawKeys.filter(x=>x.type).slice(-4));
-});
-window.addEventListener('keyup',x=>{
-    const kt=key_transform(x);
-    DownKeys[kt.code]=false;
-    RawKeys.push(kt);
-});
+const DaKeys={};/* fields are assignable, object isn't */
+const key_handler=x=>{
+    const kf=key_filter(x);
+    DaKeys[kf.code]=kf;
+    if(x.type==='keydown'){
+        /* call preventDefault() on everything but Chrome dev tools shortcut (Chrome OS and OSX) */
+        if(!(kf.code==='KeyI'&&kf.modifier===10||kf.modifier===5)){x.preventDefault()}
+        console.table(DaKeys);
+    }
+};
+window.addEventListener('keydown',key_handler);
+window.addEventListener('keyup',key_handler);
 
-let RawKeys=[],DownKeys={};
-const resize_handler=c=>{
+/* Window -- load, resize */
+const pix_ratio_fix=c=>{
     const dpr=window.devicePixelRatio, h=window.innerHeight, w=window.innerWidth;
+    c.canvas.height=c.canvas.height;
     [c.canvas.height,c.canvas.width]=[h,w].map(x=>dpr*x);
     [c.canvas.style.height,c.canvas.style.width]=[h,w].map(x=>x+'px');
+    c.font='16px monospace';/* has to be set after modifying c.canvas.anything */
     c.scale(dpr,dpr);
-    testwrite(c); // DEBUG
 };
 const ctx=document.getElementById('c').getContext('2d');
-window.addEventListener('resize',()=>resize_handler(ctx));
-window.addEventListener('DOMContentLoaded',()=>resize_handler(ctx));
+window.addEventListener('resize',()=>pix_ratio_fix(ctx));
+window.addEventListener('load',()=>{
+    pix_ratio_fix(ctx);
+    do_render(ctx); // testing
+});
