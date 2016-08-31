b8d942e0527d8f26bcc7fa84c78bdaf17948ccaa
hoosieree <ashroyer@140-182-217-180.dhcp-bl.indiana.edu>
2016-08-31 14:28:35 -0400

    Better README

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const do_render=c=>{
 
 
 /* update : AnyEvent -> Action */
-var glob=[];
+let glob=[];
 const update=perf_timestamp=>{
     glob=sort_by('timestamp',DaKeys);
 };
@@ -22,23 +22,23 @@ const update=perf_timestamp=>{
 /* Events -- keyboard and mouse */
 const DaKeys={};/* customized KeyboardEvents */
 const key_handler=x=>{
-    const kf={/* key_filter : KeyboardEvent -> RawKey */
+    const kf={/* KeyboardEvents, filtered */
         key:x.key,
         code:x.code,
         timestamp:x.timeStamp|0,
         type:x.type==='keydown'|0,
         mod:['altKey','ctrlKey','metaKey','shiftKey']
             .map(y=>x[y]|0)
-            .reduce((a,b,i,arr)=>a+b*Math.pow(2,arr.length-1-i),0),
+            .reduce((a,b,i,arr)=>a+b*Math.pow(2,arr.length-1-i),0)
     };
     DaKeys[x.code]=kf;
     if(kf.type==='keydown'){
-        let p={
+        let allow_default={
             'KeyI':[5,10],
             'KeyR':[2,4],
         }[kf.code];
         /* call preventDefault() on everything EXCEPT the chords listed above. */
-        p?p.every(m=>kf.mod!==m):true && x.preventDefault();
+        allow_default?allow_default.every(m=>kf.mod!==m):true && x.preventDefault();
     }
     requestAnimationFrame(update);
 };
@@ -47,16 +47,17 @@ window.addEventListener('keyup',key_handler);
 
 
 /* Window -- load, resize */
-const pix_ratio_fix=c=>{
+const pixel_ratio_fix=(c,cc)=>{
     const dpr=window.devicePixelRatio, h=window.innerHeight, w=window.innerWidth;
-    [c.canvas.height,c.canvas.width]=[h,w].map(x=>dpr*x);
-    [c.canvas.style.height,c.canvas.style.width]=[h,w].map(x=>x+'px');
-    c.font='16px monospace';/* has to be set after modifying c.canvas.anything */
+    [cc.height,cc.width]=[h,w].map(x=>dpr*x);
+    [cc.style.height,cc.style.width]=[h,w].map(x=>x+'px');
+    //c.font='16px monospace';/* Must set AFTER modifying canvas! */
+    c.font='16px "Source Code Pro for Powerline"';/* Must set AFTER modifying canvas! */
     c.scale(dpr,dpr);
 };
-const ctx=document.getElementById('c').getContext('2d');
-window.addEventListener('resize',()=>pix_ratio_fix(ctx));
+const canvas=document.getElementById('c'), ctx=canvas.getContext('2d');
+window.addEventListener('resize',()=>pixel_ratio_fix(ctx,canvas));
 window.addEventListener('load',()=>{
-    pix_ratio_fix(ctx);
+    pixel_ratio_fix(ctx,canvas);
     do_render(ctx); // testing
 });
