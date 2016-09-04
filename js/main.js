a24840869b9099722bb625e3c8dfcff20ddbb4d2
hoosierEE <ashroyer@gmail.com>
2016-09-04 13:33:36 -0400

    convert docs to line-per-sentence

--- js/main.js
+++ js/main.js
@@ -11,41 +11,40 @@ const render=c=>{
 };
 
 /* update : AnyEvent -> Action */
-const update=perf_timestamp=>{
+const update=(perf_now,iks,ikc)=>{
     /* what kind of keyboard input did we just get? */
-    console.table(zip(KQ).slice(-10));// sequence
-    console.log(Array.from(KP));// chords
+    console.table(zip(iks).slice(-10));// end of sequence
+    console.log(Array.from(ikc));// chords
 };
 
 /* Events -- keyboard and mouse */
-const KP=new Set();/* Keys Pressed */
-const KQ=[[],[],[],[],[]];/* KeyDown Queue -- arrays of key, code, timestamp, mod */
+const INPUT={
+    KC:new Set(), /* Key Chord */
+    KS:[[],[],[],[],[]],/* Key Sequence */
+    KST:'key code timestamp down mod'.split(' '), /* Key Sequence types */
+};
 const key_handler=x=>{
-    const fk={/* filtered KeyboardEvents */
-        key:x.key,
-        code:x.code,
-        timestamp:x.timeStamp|0,
-        down:x.type==='keydown'|0,
+    const rk={/* 'reduced' KeyboardEvents */
+        key:x.key, code:x.code, timestamp:x.timeStamp|0, down:x.type==='keydown'|0,
         mod:['altKey','ctrlKey','metaKey','shiftKey']
             .map(y=>x[y]|0)
-            .reduce((a,b,i,arr)=>a+b*Math.pow(2,arr.length-1-i),0)
+            .reduce((a,b,i,arr)=>a+b*Math.pow(2,arr.length-1-i),0),
     };
-    /* update KP here so requestAnimationFrame always deals with the same facts */
-    fk.down&&KP.add(fk.code)||KP.delete(fk.code);
-    if(fk.down){
+    /* update KC here so requestAnimationFrame always deals with the same facts */
+    rk.down&&INPUT.KC.add(rk.code)||INPUT.KC.delete(rk.code);
+    if(rk.down){
         /* Call preventDefault() on everything EXCEPT the chords listed below.
            NOTE: These chords can only be 1 non-mod key, plus 1 or more mod keys. */
         let ok_chords={
             'KeyI':[5,10],
             'KeyR':[2,4],
             /* optionally whitelist more keyboard shortcuts here */
-        }[fk.code];
-        (ok_chords?ok_chords.every(m=>fk.mod!==m):true) && x.preventDefault();
+        }[rk.code];
+        (ok_chords?ok_chords.every(m=>rk.mod!==m):true) && x.preventDefault();
 
-        /* push each field to their respective fields in KQ array */
-        const KQI=Object.keys(fk);
-        KQI.map(x=>KQ[KQI.indexOf(x)].push(fk[x]));
-        requestAnimationFrame(update);
+        /* push each field to their respective fields in KS array */
+        INPUT.KST.forEach(x=>INPUT.KS[INPUT.KST.indexOf(x)].push(rk[x]));
+        requestAnimationFrame((t)=>update(t,INPUT.KS,INPUT.KC));
     }
 };
 window.addEventListener('keydown',key_handler);
