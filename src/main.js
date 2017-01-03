c3fac858dc619979d58e8befef6a195cd9ac7b5f
hoosieree <ashroyer@gmail.com>
2017-01-03 09:11:10 -0500

    mutation to the rescue

--- src/main.js
+++ src/main.js
@@ -1,35 +1,16 @@
-/* main.js -- Entry point; wraps the browser.
-   All I/O goes through here:
-   - persistent state
-   - keyboard/mouse input
-   - drawing to screen
-   - config file load/save */
+/* main.js -- Entry point; wraps the browser.*/
 const ctx=document.getElementById('c').getContext('2d');
+const wui=WIMUI();
+const update=(ms)=>{wui.update(IN);};
 
-/* render : String -> IO()
-   TODO change to (render : Model -> IO()) */
-const render=(lines)=>{
-    ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
-    let pos=20; lines.replace(/\. +/g,'.\n').split('\n').forEach(l=>{ctx.fillText(l,20,pos+=30);});
-};
-
-let wui=WIMUI();
-
-const update=(performance_now)=>{
-    // console.log(Array.from(IN.KC));
-    wui.update(IN);
-};
-
-
-let IN={
+const IN={
     KC:new Set(),/* {KeyChord} */
     KS:[[],[],[],[]],/* [[Key],[Code],[Millis],[ModCode]] */
     KS_MAXLEN:10
 };
 
 /* key_handler : KeyboardEvent -> u1 -> IO() */
-const key_handler=(ev)=>{
-    let is_keydown=ev.type=='keydown';
+const key_handler=(ev,is_keydown)=>{
         IN.KC[is_keydown?'add':'delete'](ev.code);
     if(is_keydown){
         const rk=[ev.key, ev.code, ev.timeStamp|0,
@@ -43,27 +24,28 @@ const key_handler=(ev)=>{
         requestAnimationFrame(update);
     }
 };
-window.addEventListener('keydown',key_handler);
-window.addEventListener('keyup',key_handler);
+window.addEventListener('keydown',(e)=>key_handler(e,1));
+window.addEventListener('keyup',(e)=>key_handler(e,0));
 
 /* Window -- load, resize */
 const pixel_ratio_fix=(s)=>{
     const dpr=window.devicePixelRatio, h=window.innerHeight, w=window.innerWidth;
     [ctx.canvas.height,ctx.canvas.width]=[h,w].map(x=>dpr*x);
     [ctx.canvas.style.height,ctx.canvas.style.width]=[h,w].map(x=>x+'px');
-    /* Set font size AFTER modifying canvas! */
-    //ctx.font='18px "Source Code Pro for Powerline"'; //ctx.font='16px monospace';
-    ctx.font=(9*dpr)+'px "Source Code Pro for Powerline"';
+    // fonts AFTER canvas mod
+    ctx.font=(18*dpr)+'px "Source Code Pro for Powerline"';
     ctx.scale(dpr,dpr);
 };
-let str="This is Ginger. She is a linx and has a glowing blue mane that she shakes to get warm. She likes to make fire sparks out of her tail. Her favorite thing to eat is peppers so she can make her sparks. Ginger lives with her linx family. She has friends that are birds. They live together in the forest. The trees are magical so they don't get burned down. She likes living in the forest. ";
 
-window.addEventListener('load',()=>{
-    pixel_ratio_fix(str);
-    render(str);
-});
-window.addEventListener('resize',()=>{
-    pixel_ratio_fix(str);
-    render(str);
-});
+/* render : String -> IO() TODO change to (render : Model -> IO()) */
+const render=(lines)=>{
+    ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
+    let pos=20; lines.replace(/\. +/g,'.\n').split('\n').forEach(l=>{ctx.fillText(l,20,pos+=30);});
+};
+
+// testing
+let str="This is Ginger. She is a linx and has a glowing blue mane that she shakes to get warm. She likes to make fire sparks out of her tail. Her favorite thing to eat is peppers so she can make her sparks. Ginger lives with her linx family. She has friends that are birds. They live together in the forest. The trees are magical so they don't get burned down. She likes living in the forest. ";
+const winevts=()=>{pixel_ratio_fix(str);render(str);}
+window.addEventListener('load',winevts);
+window.addEventListener('resize',winevts);
 
--- src/wimui.js
+++ src/wimui.js
@@ -1,29 +1,24 @@
-/* Vim-sytle UI */
 const WIMUI=()=>{
-    /* simple objects */
-    let current_state='normal', multiplier=1, multiplier_str=['',''];
-    const get_multiplier=(multstr)=>{/* [String] -> Int */ return multstr.reduce((a,b)=>a*(b||1),1);},
-          reset_multiplier=()=>{multiplier=1; multiplier_str=['',''];},
-          initial_state='normal';
-
-    /* State machine tries to match: Chords, then Sequences, then Atoms. */
-    const chord={
+    const chord={/* Highest priority match. */
         'C-[':{act:'escape',code:'BracketLeft',mods:[2]},
         'C-g':{act:'escape',code:'KeyG',mods:[2]},
         'C-d':{act:'motion',code:'KeyD',mods:[2]},
         'C-u':{act:'motion',code:'KeyU',mods:[2]},
-        'C-v':{act:'visual',code:'KeyV',mods:[2]}
-    };
+        'C-v':{act:'visual',code:'KeyV',mods:[2]}};
 
-    const seq={/* {Seq:{Action,ReverseName,MinMilliseconds?}} */
-        'fd':{act:'escape',rn:'df',dt:500},
-        'cs':{act:'csurround',rn:'sc'},
-        'ds':{act:'dsurround',rn:'sd'},
-        'ys':{act:'ysurround',rn:'sy'}
-    };
+    const seq=(()=>{/* {Seq:{Action,ReverseName,MinMilliseconds?}} */
+        const ts={
+            'fd':{act:'escape',dt:500},
+            'gg':{act:'text_object'},
+            'cs':{act:'csurround'},
+            'ds':{act:'dsurround'},
+            'ys':{act:'ysurround'}};
+        Object.keys(ts).map(x=>ts[x].rn=[...x].reverse().join(''));
+        return ts;
+    })();
 
     const atom=(()=>{/* Eventually becomes {Char:[Type]} */
-        let xs={/* Starts off as {Type:[Char]} */
+        let xs={/* Begins as {Type:[Char]} */
             ascii:'',
             bracket:'[{()}]',
             edit:'oOpPrxX',
@@ -43,133 +38,127 @@ const WIMUI=()=>{
             verb:'cdy',
             visual:'vV'
         };
-        let as='';
-        for(let i=32;i<127;++i){as+=String.fromCharCode(i);}
-        xs.ascii=as;
-        /* tag doesn't start empty */
+        for(let i=32;i<127;++i){xs.ascii+=String.fromCharCode(i);}
         for(let i=65;i<90;++i){xs.tag+=String.fromCharCode(i);}/* A-Z */
         for(let i=97;i<122;++i){xs.tag+=String.fromCharCode(i);}/* a-z */
-        let t={};
-        for(let x in xs){[...xs[x]].forEach(y=>t[y]?t[y].push(x):t[y]=[x]);}/* invert xs table */
+        let t={};for(let x in xs){[...xs[x]].forEach(y=>t[y]?t[y].push(x):t[y]=[x]);}
         t.Escape=['escape'];
         return t;
     })();
 
-    const st={/* State Tree */
-        get mult_0(){return this;},
+    const leaf=1;
+    const st=()=>({/* State Tree */
+        escape:leaf,
+        get mult_0(){// mult_0 (once), then mult_N
+            delete this.mult_0;
+            Object.defineProperty(this,'mult_N',{get: function(){return this;}});
+            return this;
+        },
         verb:{
             modifier:{
-                seek:{ascii:1},
-                text_object:1
-            },
-            motion:1,
-            seek:{ascii:1},
-            text_object:1
-        },
+                seek:{ascii:leaf},
+                text_object:leaf},
+            motion:leaf,
+            seek:{ascii:leaf},
+            text_object:leaf},
         csurround:{
             bracket:{
-                bracket:1,
-                tag:{tag_end:1}
-            },
+                bracket:leaf,
+                tag:{tag_end:leaf}},
             tag:{
-                bracket:1,
-                tag_end:1
-            }
-        },
+                bracket:leaf,
+                tag_end:leaf}},
         dsurround:{
-            bracket:1,
-            tag:{tag_end:1}
-        },
+            bracket:leaf,
+            tag:{tag_end:leaf}},
         ysurround:{
             bracket:{
-                bracket:1,
-                tag:{tag_end:1}
-            },
+                bracket:leaf,
+                tag:{tag_end:leaf}},
             modifier:{
-                seek:{ascii:{
-                    bracket:1,
-                    tag:{tag_end:1}
-                }},
+                seek:{
+                    ascii:{
+                        bracket:leaf,
+                        tag:{tag_end:leaf}}},
+                motion:{
+                    bracket:leaf,
+                    tag:{tag_end:leaf}},
                 text_object:{
-                    bracket:1,
-                    tag:{tag_end:1}
-                }
-            },
+                    bracket:leaf,
+                    tag:{tag_end:leaf}}},
             motion:{
-                bracket:1,
-                tag:{tag_end:1}
-            },
+                bracket:leaf,
+                tag:{tag_end:leaf}},
             seek:{
                 ascii:{
-                    bracket:1,
-                    tag:{tag_end:1}
-                }
-            },
+                    bracket:leaf,
+                    tag:{tag_end:leaf}}},
             text_object:{
-                bracket:1,
-                tag:{tag_end:1}
-            }
-        },
-        motion:1,
-        seek:{ascii:1},
-        text_object:1
-    };
+                bracket:leaf,
+                tag:{tag_end:leaf}}},
+        motion:leaf,
+        seek:{ascii:leaf},
+        text_object:leaf
+    });
 
-    const chord_seq_check=(n)=>{
+    const chord_check=(n)=>{
         for(let x in chord){
             const i=chord[x],
                   has_key=Array.from(n.KC).indexOf(i.code)>-1,
                   has_mod=(0>i.mods)||i.mods.some(y=>y==n.KS[3][0]);
-            if(has_key&&has_mod){
-                i.name=x;
-                return [i,null];
-            }
+            if(has_key && has_mod){i.name=x; return i;}
         }
-        const dt_check=(x,y,z)=>(!x||y)?z:null;
-        for(let x in seq){
-            const i=seq[x],
-                  has_seq=n.KS[0].join('').startsWith(i.rn);
-            if(has_seq){
-                const exceeded_timeout=i.dt>n.KS[2].slice(0,x.length).reduce((a,b)=>a-b);
-                return [null,dt_check(i.dt,exceeded_timeout,i)];
-            }
-        }
-        return [null,null];
+        return null;
     };
 
-    let fn=st;
-    const update=(input)=>{
-        [ok_chord,ok_seq]=chord_seq_check(input);
-
-        const match_state=(e,msg)=>{
-            if(fn==null){fn=st[e];}
-            else if(fn[e]!=null){fn=fn[e];}
-            else if(st[e]!=null){fn=st[e];}
-            else{fn=null;}
-            return fn!=null;
-        };
-
-        if(ok_chord!=null){
-            match_state(ok_chord.act,'bad chord');
+    const seq_check=(n)=>{
+        const dt_check=(x,y,z)=>(!x||y)?z:null,
+              dt_over=(i,x)=>i.dt>n.KS[2].slice(0,x.length).reduce((a,b)=>a-b);
+        for(let x in seq){
+            const i=seq[x], has_seq=n.KS[0].join('').startsWith(i.rn);
+            if(has_seq){return dt_check(i.dt,dt_over(i,x),i);}
         }
-        else if(ok_seq!=null){
-            match_state(ok_seq.act,'bad sequence');
+        return null;
+    };
+
+    let current=[], stt=st();
+    const state_reset=()=>{stt=st(); current=[];};
+
+    const match_state=(e)=>{
+        if(stt[e]){
+            current.push(e);
+            console.log(current);
+            console.log(`stt: ${stt} leaf: ${leaf}`);
+            if(stt[e]==leaf){
+                console.log("I'm a leaf");
+                console.log(stt);
+                state_reset();
+                return 1;
+            }
+            else{
+                stt=stt[e];
+            }
+        }
+        return 0;
+    };
+
+    const update=(input)=>{
+        let okc,oks;
+        if((okc=chord_check(input)) && match_state(okc.act)){
+            state_reset();
+        }
+        else if((oks=seq_check(input)) && match_state(oks.act)){
+            state_reset();
         }
         else{
-            let et=atom[input.KS[0][0]]||[];// console.log(et);
+            let et=atom[input.KS[0][0]]||[];
             for(let i=0;i<et.length;++i){
-                //console.log(et[i]);
-                if(match_state(et[i],'bad atom')) break;
+                if(match_state(et[i])){
+                    state_reset();
+                    break;
+                }
             }
         }
-
-        console.log(fn);
-
-        /* TODO output
-         Option 1: Accumulate actual keys and (upon successful state change)
-         export the key sequence to external handling function.
-         Option 2: Immediately pass (state, key) tuples to handling function.
-         */
     };
-    return ({update,st});
+    return ({update,st,seq});
 };
