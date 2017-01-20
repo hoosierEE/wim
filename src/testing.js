60ba926da8a9f047fe9864692f074fbe284d6df9
Alex <ashroyer@indiana.edu>
2017-01-20 17:59:08 -0500

    meh

--- src/testing.js
+++ src/testing.js
@@ -31,70 +31,69 @@ const shuffle=(array)=>{
 
 
 /* TEST! */
-const test=()=>{
-  const p=Parser(),
-        keydn=(e)=>p.key_handler(e,0),
-        keyup=(e)=>p.key_handler(e,1),
-        peck=(e)=>{const r=keydn(e);keyup(e); return r;},
-        chord=(...e)=>{const r=e.map(x=>keydn(x));e.forEach(x=>keyup(x)); return r;};
+const p=Parser(),
+      keydn=(e)=>p.key_handler(e,0),
+      keyup=(e)=>p.key_handler(e,1),
+      peck=(e)=>{const r=keydn(e);keyup(e); return r;},
+      chord=(...e)=>{const r=e.map(x=>keydn(x));e.forEach(x=>keyup(x)); return r;};
 
-  /* Mock KeyboardEvent */
-  const mke=(k,m=0,c='',ts=performance.now())=>{
-    if(!c && k>='a'&&k<='z' || k>='A'&&k<='Z'){c='Key'+k.toUpperCase();}
-    const t={code:c, key:k, timeStamp:ts, preventDefault:function(){}},
-          mods=['shiftKey','metaKey','ctrlKey','altKey'];
-    let mc=m.toString(2);while(mc.length<4){mc='0'+mc;}
-    [...mc].forEach((x,i)=>t[mods[i]]=!!Number(x));
-    return t;
-  };
-
-  /* Control-g should reset */
-  const Control=mke('Control',2,'ControlLeft'), Control_g=mke('g',2);/* Ctrl-g is a reset */
-  const reset=()=>{return cg=chord(Control,Control_g)[1];};
-  console.assert(reset().status=='quit','can reset');
-
-  const alpha_lowercase=String.fromCharCode(...iota([26]).map(x=>x+(ai('a')))),
-        mkes=[...alpha_lowercase].map(x=>mke(x,0));
-
-  const f=(n)=>{
-    return [...alpha_lowercase].map((x,i)=>{
-      let r=peck(mkes[i]);
-      if(n>0 && r.status=='continue'){reset();return x;}
-      return 0;
-    }).filter(x=>x);
-  }; pp(f(5));
-
-  /* TODO -- Depth-first search all sequences of keyboard events.
-
-   f():
-      for letter in letters.map(peck):
-        if 'continue':
-          return letter+f()
-        return letter
-
-   a (error) -> a
-   b (done) -> b
-   c (continue) -> c + f()
-   ca (error)
-   cb (done)
-   cc (done)
-   cd (error)
-   ce (done)
-   cf (continue)
-   cfa (done)
-   ...
-   cfz (done)
-   cg (error)
-   ce (done)
-   cf (continue)
-   cfa (done)
-   ...
-   cfz (done)
-   cg (error)
-   ...
-   cz (error)
-   d (continue)
-   ...
-   */
+/* Mock KeyboardEvent */
+const mke=(k,m=0,c='',ts=performance.now())=>{
+  if(!c && k>='a'&&k<='z' || k>='A'&&k<='Z'){c='Key'+k.toUpperCase();}
+  const t={code:c, key:k, timeStamp:ts, preventDefault:function(){}},
+        mods=['shiftKey','metaKey','ctrlKey','altKey'];
+  let mc=m.toString(2);while(mc.length<4){mc='0'+mc;}
+  [...mc].forEach((x,i)=>t[mods[i]]=!!Number(x));
+  return t;
 };
-test();
+
+/* Control-g should reset */
+const Control=mke('Control',2,'ControlLeft'), Control_g=mke('g',2);/* Ctrl-g is a reset */
+const reset=()=>{return cg=chord(Control,Control_g)[1];};
+console.assert(reset().status=='quit','can reset');
+
+const alpha_lowercase=String.fromCharCode(...iota([26]).map(x=>x+(ai('a')))),
+      mkes=[...alpha_lowercase].map(x=>mke(x,0));
+
+const check_seq=(str)=>{
+  const evts=[...str].map(x=>mke(x,0));
+  for(let i in evts){
+    let r=peck(evts[i]);
+    if(r.status!='continue'){reset(); return 0;}
+  } return 1;
+};
+
+
+
+/* TODO -- Depth-first search all sequences of keyboard events.
+
+ f():
+ for letter in letters.map(peck):
+ if 'continue':
+ return letter+f()
+ return letter
+
+ a (error) -> a
+ b (done) -> b
+ c (continue) -> c + f()
+ ca (error)
+ cb (done)
+ cc (done)
+ cd (error)
+ ce (done)
+ cf (continue)
+ cfa (done)
+ ...
+ cfz (done)
+ cg (error)
+ ce (done)
+ cf (continue)
+ cfa (done)
+ ...
+ cfz (done)
+ cg (error)
+ ...
+ cz (error)
+ d (continue)
+ ...
+ */
