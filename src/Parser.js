08f166645f85e49522d5313512b9e280f9325fef
Alex <ashroyer@indiana.edu>
2017-01-27 16:46:45 -0500

    parseInt adds readability (and 1 extra byte)

--- src/Parser.js
+++ src/Parser.js
@@ -177,7 +177,7 @@ const Parser=(logging=0)=>{
   const key_handler=(a,b)=>{
     KC[b?'delete':'add'](a.code); if(b){return null;}
     const evt={key:a.key, code:a.code, ts:a.timeStamp|0,
-               mods:['altKey','ctrlKey','metaKey','shiftKey'].reduce((x,y,z)=>x|((a[y]|0)<<z),0),
+               mods:parseInt(['shiftKey','metaKey','ctrlKey','altKey'].map(x=>a[x]|0).join(''),2),
                chord:Array.from(KC)},
           ad={'KeyI':[10,5],'KeyR':[2,4]}[evt.code];/* allow default */
     if(ad && ad[navigator.platform==='MacIntel'|0]===evt.mods){return null;}
