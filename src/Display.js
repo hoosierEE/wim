189c4a5524ae3b6dbbf1c6684f8b50ed7157e90c
Alex <ashroyer@indiana.edu>
2017-01-30 10:59:54 -0500

    big rename and move around
    practicing README-driven development

---
+++ src/Display.js
@@ -0,0 +1,23 @@
+const Display=()=>{/* Display the app itself. */
+  const canvas=document.getElementById('c'), context=canvas.getContext('2d');
+
+  const reset=()=>{
+    const dpr=window.devicePixelRatio, h=window.innerHeight, w=window.innerWidth;
+    context.scale(dpr,dpr);
+    [canvas.height,canvas.width]=[h,w].map(x=>dpr*x);
+    [canvas.style.height,canvas.style.width]=[h,w].map(x=>x+'px');
+
+    /* Set fonts AFTER canvas mod! */
+    context.font=(18*dpr)+'px monospace';
+  };
+
+  const render=(doc)=>{/* TextDocument -> Canvas () */
+    context.clearRect(0,0,canvas.width,canvas.height);
+    let pos=20;
+    for(let i in doc.lines){context.fillText(doc.nth_line(i),20,pos+=30);}
+  };
+
+  reset();
+
+  return ({reset,render});
+};
---
+++ test/testing.js
@@ -0,0 +1,42 @@
+/* TEST! */
+const p=Parser(),
+      keydn=(a)=>p.key_handler(a,0),
+      keyup=(a)=>p.key_handler(a,1),
+      peck=(a)=>{const r=keydn(a);keyup(a); return r;},
+      chord=(...a)=>{const r=a.map(x=>keydn(x));a.forEach(x=>keyup(x)); return r;};
+
+/* Mock KeyboardEvent */
+const mke=(k,m=0,c='',ts=performance.now())=>{
+  if(!c && k>='a'&&k<='z' || k>='A'&&k<='Z'){c='Key'+k.toUpperCase();}
+  const t={code:c, key:k, timeStamp:ts, preventDefault:function(){}},
+        mods=['shiftKey','metaKey','ctrlKey','altKey'];
+  let mc=m.toString(2); while(mc.length<4){mc='0'+mc;}
+  [...mc].forEach((x,i)=>t[mods[i]]=!!Number(x));
+  return t;
+};
+
+/* Control-g should reset */
+const Control=mke('Control',2,'ControlLeft'), Control_g=mke('g',2);/* Ctrl-g should reset */
+const reset=()=>{return cg=chord(Control,Control_g)[1];};
+console.assert(reset().status=='quit','Ctrl+G should reset');
+
+/* Long sequences of characters should be possible. */
+const alphabet='abcdefghijklmnopqrstuvwxyz',
+      mkes=[...alphabet].map(x=>mke(x,0));
+
+const test=(str)=>{
+  reset(); for(let s of str){
+    if(peck(mkes[alphabet.indexOf(s)]).status!=='continue'){return false;}
+  } return true;
+};
+
+const addone=(str='')=>{
+  let r=[]; for(let a of alphabet){
+    if(test(str+a)){r.push(str+a);}
+  } return r.length?r:str;
+};
+
+const flatten=(x)=>x.reduce((a,b)=>a.concat(Array.isArray(b)?flatten(b):b),[]);
+const addall=(x)=>flatten(x.map(addone));
+const longseq=(x,n)=>{while(n-->0){x=addall(x);} return x;};
+console.assert(Math.max(...longseq(['c','d'],5).map(x=>x.length),4),'long sequences (>4) should exist for verbs');
