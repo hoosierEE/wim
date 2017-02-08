827374225e7c6527c4c4a36aa9eb16538b5aa6ee
Alex <ashroyer@indiana.edu>
2017-02-08 16:20:12 -0500

    cursor sometimes working

--- src/Core.js
+++ src/Core.js
@@ -1,21 +1,19 @@
 /* Core.js: input | core | render */
 const Core=()=>{/* String (looks like an array of lines). */
   const
-  my={str:'', starts:[], lines:[]},/* Document (string) */
+  my={str:''},/* Document (string) */
   cursor={line:0, col:0, maxcol:0, height:1.0, scroll:false, color:'normal'},
-  document={line:0},
 
   /* Document */
   all_lines=()=>my.str.match(/^.*/mg),
-  idx=(a)=>{/* number -> valid index into [lines] */
-    a|=0;const l=all_lines().length;
-    if(0<=a&&a<l){return a;}
-    if(a>=l){return l-1;}
-    return Math.max(0,a+l);
+  idx=(n)=>{/* number -> valid index into [lines] */
+    n|=0;const l=all_lines().length;
+    if(0<=n&&n<l){return n;}
+    if(n>=l){return l-1;}
+    return Math.max(0,n+l);
   },
-  lines=(a,b)=>{/* (a+i.b) { cutLF lines */
-    const c=idx(a);
-    return all_lines().slice(c,Math.max(c,c+b|0));
+  lines=(start,count)=>{/* (start+i.count) { cutLF lines */
+    const c=idx(start); return all_lines().slice(c,Math.max(c,c+count|0));
   },
   put=(a)=>{my.str=a;},
   ins=(a,b)=>{},/* TODO insert string (a) at position(s) (b) */
@@ -24,20 +22,20 @@ const Core=()=>{/* String (looks like an array of lines). */
   /* Storage */
   save=(a)=>{},/* TODO Send a save/commit request to persistent storage. */
 
-  /* Plugin */
+  /* Plugins */
   dispatch=(a)=>{},/* TODO given a ParsedEvent, possibly invoke some functions */
 
   /* Listener */
-  hears=(a)=>{
-    /* TODO dispatch(a) to replace all of this: */
+  hears=(parsed)=>{
+    /* TODO plugins.dispatch(parsed) replaces all of this */
     let heard=false;
-    if(a && 'done'===a.status){
-      let n=a.keys.length-1, last=[a.keys[n], a.mods[n], a.part[n]];
+    if(parsed && 'done'===parsed.status){
+      let n=parsed.keys.length-1, last=[parsed.keys[n], parsed.mods[n], parsed.part[n]];
       if(last[2]==='motion'){
         heard=true;
         switch(last[0]){
-        case'j':cursor.line+=1;break;
-        case'k':cursor.line-=1;break;
+        case'j':cursor.line=Math.min(all_lines().length, cursor.line+1);break;
+        case'k':cursor.line=Math.max(0,cursor.line-1);break;
         case'h':cursor.col-=1;break;
         case'l':cursor.col+=1;break;
         }
@@ -45,5 +43,5 @@ const Core=()=>{/* String (looks like an array of lines). */
     } return heard;
   };
 
-  return ({lines, put, hears, cursor, document});
+  return ({lines, put, hears, cursor, all_lines});
 };
--- src/Display.js
+++ src/Display.js
@@ -1,7 +1,9 @@
 const Display=()=>{
-  let delta_y, initial_y, dpr, lines_per_screen;/* Depends on user-adjustable window zoom, font size params. */
+  let LH, LS, TOP=0, BASELINE, dpr;/* Depends on user-adjustable window zoom, font size params. */
   const
-  can=document.getElementById('c'), ctx=can.getContext('2d'), cfg={border:20,font:16},/* Static (for now). */
+  can=document.getElementById('c'),
+  ctx=can.getContext('2d'),
+  cfg={border:20,font:16,pad:5},/* Static (for now). */
 
   draw_cursor=({x,y,w,h},color)=>{
     ctx.save();
@@ -10,41 +12,53 @@ const Display=()=>{
     ctx.restore();
   },
 
-  cursor_position=(a)=>{/* {line,column} => {x,y,w,h,scroll?} */
+  render=(core)=>{/* Text -> Canvas () */
+    ctx.clearRect(0,0,can.width,can.height);
     // x: cfg.border + cursor column
     // y: cfg.border + cursor line * cursor height
-    // w: current letter
+    // w: width of current letter
     // h: line height
-    let r={x:cfg.border,y:cfg.border+0.25*delta_y,w:600,h:delta_y},
-        dy=a.cursor.line*delta_y;
 
-    if(dy<r.y){/* scroll up */}
-    if(dy>=r.y+lines_per_screen*delta_y+cfg.border){/* scroll down */}
+    /* scroll? */
+    const bpad=TOP+LS-cfg.pad;
+    if(cfg.pad*2>LS){TOP=core.cursor.line;}
+    else{
+      const len=core.all_lines().length;
+      if(core.cursor.line>bpad){TOP+=Math.min(len,core.cursor.line-bpad);}
+      else if(core.cursor.line<TOP+cfg.pad){TOP=Math.max(0,core.cursor.line-cfg.pad);}
+    }
 
-    // let r={x:0,y:0,w:0,h:0,scroll:true};
-    return r;
-  },
+    const lines=core.lines(TOP,LS),
+          cl=lines[core.cursor.line-TOP],
+          cxl=cl.slice(0,core.cursor.col),
+          cxr=cl.slice(0, core.cursor.col+1),
+          [left,right]=[cxl,cxr].map(x=>ctx.measureText(x).width);
+
+    draw_cursor({
+      x: cfg.border+left,
+      y: cfg.border+LH*(core.cursor.line-TOP+0.25),
+      w: right-left,
+      h: LH
+    },'orange');
+
+    /* Doc should tell what line/column has the cursor, Display should turn those into x/y/w/h. */
+    lines.forEach((x,i)=>ctx.fillText(x, cfg.border, BASELINE+i*LH));
+  };
 
   update=()=>{
-    dpr=window.devicePixelRatio, h=window.innerHeight, w=window.innerWidth;
-    [can.height,can.width]=[h,w].map(x=>x*dpr);
-    delta_y=cfg.font*dpr*1.5;
-    lines_per_screen=can.height/delta_y;
-    initial_y=cfg.border+delta_y;
+    dpr=window.devicePixelRatio;
+    h=window.innerHeight;
+    w=window.innerWidth;
+    can.height=h*dpr;
+    can.width=w*dpr;
+
+    LH=cfg.font*dpr*1.5;/* line height */
+    LS=can.height/LH|0;/* lines per screen */
+    BASELINE=cfg.border+LH;
     /* Set font AFTER canvas scaling! */
     ctx.font=cfg.font*dpr+'px serif';
   },
 
-  render=(a)=>{/* Text -> Canvas () */
-    ctx.clearRect(0,0,can.width,can.height);
-
-    draw_cursor(cursor_position(a),'orange');
-
-    /* Doc should tell what line/column has the cursor, Display should turn those into x/y/w/h. */
-    const lines=a.lines(a.document.line,lines_per_screen);
-    lines.forEach((x,i)=>ctx.fillText(x, cfg.border, initial_y+i*delta_y));
-  };
-
   update();
-  return ({update,render,ctx});
+  return ({update,render});
 };
