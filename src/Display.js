16787b0cdfdfa7b510cbf3cf49d5fd7b95ff5014
Alex <ashroyer@indiana.edu>
2017-02-10 17:14:57 -0500

    better scroll

--- src/Display.js
+++ src/Display.js
@@ -1,12 +1,11 @@
 const Display=()=>{
-  let LH, LS, TOP=0, BASELINE, dpr;/* Depends on user-adjustable window zoom, font size params. */
+  let LH, LS, VS, TOP=0, dpr=1;/* Depends on user-adjustable window zoom, font size params. */
   const
   can=document.getElementById('c'),
   ctx=can.getContext('2d'),
-  cfg={border:20,font:16,pad:5},/* Static (for now). */
+  cfg={border:20, font:16, pad:5, line_spacing:1},/* Static (for now). */
 
   draw_cursor=({x,y,w,h},color)=>{
-    console.log(w);
     ctx.save();
     ctx.fillStyle=color;
     ctx.fillRect(x,y,w,h);
@@ -15,50 +14,43 @@ const Display=()=>{
 
   render=(core)=>{/* Text -> Canvas () */
     ctx.clearRect(0,0,can.width,can.height);
-    // x: cfg.border + cursor column
-    // y: cfg.border + cursor line * cursor height
-    // w: width of current letter
-    // h: line height
 
     /* scroll? */
-    const bpad=TOP+LS-cfg.pad;
-    if(cfg.pad*2>LS){TOP=core.cursor.line;}
-    else{
-      const len=core.all_lines().length;
-      if(core.cursor.line>bpad){TOP+=Math.min(len,core.cursor.line-bpad);}
-      else if(core.cursor.line<TOP+cfg.pad){TOP=Math.max(0,core.cursor.line-cfg.pad);}
-    }
+    if(core.cursor.line>TOP+VS){TOP=core.cursor.line-VS;}
+    else if(core.cursor.line<TOP){TOP=core.cursor.line;}
 
     const
     lines=core.lines(TOP,LS),
-    cl=lines[core.cursor.line-TOP],
-    cxl=cl.slice(0,core.cursor.col),
-    cxr=cl.slice(0, core.cursor.col+1),
-    [left,right]=[cxl,cxr].map(x=>ctx.measureText(x).width);
+    cl=lines[core.cursor.line-TOP],/* cursor's line */
+    [L,R]=[0,1].map(x=>ctx.measureText(cl.slice(0,core.cursor.col+x)).width);
 
     draw_cursor({
-      x: cfg.border+left,
-      y: cfg.border+LH*(core.cursor.line-TOP+0.25),
-      w: Math.max(4,right-left),
-      h: LH
-    },'orange');
+      x: cfg.border+L-2,
+      y: cfg.border+LH*(core.cursor.line-TOP+cfg.line_spacing/4)-2,
+      w: Math.max(4,R-L)+4,
+      h: LH+4
+    },'hsl(0,100%,40%)');
 
-    /* Doc should tell what line/column has the cursor, Display should turn those into x/y/w/h. */
-    lines.forEach((x,i)=>ctx.fillText(x, cfg.border, BASELINE+i*LH));
+    draw_cursor({
+      x: cfg.border+L,
+      y: cfg.border+LH*(core.cursor.line-TOP+cfg.line_spacing/4),
+      w: Math.max(4,R-L),
+      h: LH
+    },'hsl(9,100%,80%)');
+
+    lines.forEach((x,i)=>ctx.fillText(x, cfg.border, cfg.border+LH+i*LH));
   };
 
   update=()=>{
     dpr=window.devicePixelRatio;
-    h=window.innerHeight;
-    w=window.innerWidth;
-    can.height=h*dpr;
-    can.width=w*dpr;
+    can.height=window.innerHeight*dpr;
+    can.width=window.innerWidth*dpr;
 
-    LH=cfg.font*dpr*1.5;/* line height */
-    LS=can.height/LH|0;/* lines per screen */
-    BASELINE=cfg.border+LH;
-    /* Set font AFTER canvas scaling! */
-    ctx.font=cfg.font*dpr+'px serif';
+    LH=cfg.font*dpr*cfg.line_spacing;/* line height */
+    LS=Math.ceil((can.height-cfg.border*2)/LH);/* lines (including partial) */
+    VS=Math.floor((can.height-cfg.border*2-LH)/LH);/* fully visible lines */
+
+    ctx.font=cfg.font*dpr+'px monospace';
   },
 
   update();
